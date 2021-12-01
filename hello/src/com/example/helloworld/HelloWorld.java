d5b19b8f47d33b89ad3d98e1fd24478562f716bb
Musushili <1404817462@qq.com>
2021-12-01 21:05:58 +0800

    Message

---
+++ hello/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.helloworld;
+
+import static java.lang.System.*;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
